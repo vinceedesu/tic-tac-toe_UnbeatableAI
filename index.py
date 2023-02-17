bba7b761af5831ee09230f2d9a3fee853cbacc47
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-18 01:19:23 +0800

    finished building the AI

--- index.py
+++ index.py
@@ -92,18 +92,31 @@ class AI:
         
         #player 1 win
         if case == 1:
-            return 1
+            return 1, None
         
         #player 2 win
         if case == 2:
-            return -1 
+            return -1, None
         
         #draw
-        elif board.isfull():
-            return 0
+        elif board.isFull():
+            return 0, None
         
         if maximizing:
-            pass
+            max_eval = -100
+            best_move = None
+            empty_sqrs = board.get_empty_sqrs()
+            
+            for (row, col) in empty_sqrs:
+                temp_board =  copy.deepcopy(board)
+                temp_board.markSquare(row, col, 1)
+                eval = self.minimax(temp_board, False)[0]
+                
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = (row, col)
+                    
+            return max_eval, best_move
         
         elif not maximizing:
             min_eval = 100
@@ -113,17 +126,24 @@ class AI:
             for (row, col) in empty_sqrs:
                 temp_board =  copy.deepcopy(board)
                 temp_board.markSquare(row, col, self.player)
-                self.minimax(temp_board, True, )
+                eval = self.minimax(temp_board, True)[0]
+                
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = (row, col)
+                    
+            return min_eval, best_move
         
         
     
     def eval(self, main_board):
         if self.level == 0:
+            eval =  'random'
             move = self.rnd(main_board)
         else:
             # minimax algo
-            self.minimax(main_board, False)
-    
+            eval, move = self.minimax(main_board, False)
+        print(f'AI chose to mark the square in pos {move} with an eval of {eval}')
         return move
 class Game:
     
