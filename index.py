1348b4ad2322da2338352c9d3bef84661e227c19
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-17 21:29:23 +0800

    class board function

--- index.py
+++ index.py
@@ -20,14 +20,59 @@ class Board:
     
     def __init__(self):
         self.squares = np.zeros( (ROWS, COLS) )
+        self.empty_sqrs = self.squares
+        self.marked_sqrs = 0
         
+    def final_state(self):
+        """
+            @return 0 if no win yet
+            @return 1 if player 1 win
+            @return 2 if player 2 win
+        """
+        
+        # vertical wins
+        for col in range(COLS):
+            if self.squares[0][col] == self.squares[1][col] == self.squares[2][col] != 0:
+                return self.squares[0][col]
+        
+        #horizontal wins 
+        for row in range(ROWS):
+            if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] != 0:
+                return self.squares[row][0]
+        
+        #diag  decs win
+        if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
+            return self.squares[1][1]
+        
+        #diag asc win 
+        if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:
+            return self.squares[1][1]
+        
+        #no win yet
+        return 0
     
     def markSquare(self, row, col, player):
         self.squares[row][col] = player
+        self.marked_sqrs += 1
         
     def emptySquare(self, row, col):
         return self.squares[row][col] == 0
 
+    def get_empty_sqrs(self):
+        empty_sqrs = []
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.emptySquare(row, col):
+                    empty_sqrs.append( (row, col) )
+
+        return empty_sqrs
+    
+    def isFull(self):
+        return self.marked_sqrs == 9
+    
+    def isempty(self):
+        return self.marked_sqrs == 0
+    
 
 
 class Game:
