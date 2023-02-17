304a84560670e48c5c52ed96c6fbd2cd3c6a99cc
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-17 16:41:58 +0800

    add new class and obj

--- constants.py
+++ constants.py
@@ -1,9 +1,17 @@
 # Display screen
-
 # changed screen size
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 501
+HEIGHT = 501
 
-# changed screen color
+ROWS = 3
+COLS = 3
+
+#Changed square size width/3
+SQUARE_SIZE = WIDTH // COLS
+
+line_WIDTH = 10
+
+# changed color scheme
 BG_Color = (69, 42, 60)
+line_Color = (105,230,18)
 
--- index.py
+++ index.py
@@ -15,8 +15,25 @@ pygame.display.set_caption('Tic Tac Toe')
 screen.fill( BG_Color )
 
 
+class Game:
+    
+    def __init__(self): 
+        self.show_lines()
+
+    def show_lines(self):
+        #vertical lines
+        pygame.draw.line(screen,line_Color,(SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), line_WIDTH)
+        pygame.draw.line(screen,line_Color,(SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), line_WIDTH)
+        
+        # horizontal lines
+        pygame.draw.line(screen,line_Color,(SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), line_WIDTH)
+        pygame.draw.line(screen,line_Color,(SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), line_WIDTH)
+
 # main function
 def main():
+    
+    game = Game()
+     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
