f2f1aab7f1c3ef81531c3bf7886bed8adf090554
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-17 15:30:54 +0800

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,20 @@ import sys
 import pygame
 
 from constants import *
+
 # initialize pygame
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Tic Tac Toe')
+
+
+# main function
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+main()
