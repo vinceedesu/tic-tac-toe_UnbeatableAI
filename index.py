5460d5813aaccd2a2752ebc92775489e5e0a1ea3
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-17 17:10:57 +0800

    Added second player

--- index.py
+++ index.py
@@ -20,7 +20,13 @@ class Board:
     
     def __init__(self):
         self.squares = np.zeros( (ROWS, COLS) )
-        print(self.squares)
+        
+    
+    def markSquare(self, row, col, player):
+        self.squares[row][col] = player
+        
+    def emptySquare(self, row, col):
+        return self.squares[row][col] == 0
 
 
 
@@ -28,6 +34,7 @@ class Game:
     
     def __init__(self): 
         self.board = Board()
+        self.player = 1
         self.show_lines()
 
     def show_lines(self):
@@ -38,17 +45,34 @@ class Game:
         # horizontal lines
         pygame.draw.line(screen,line_Color,(0, SQUARE_SIZE), (WIDTH, SQUARE_SIZE), line_WIDTH)
         pygame.draw.line(screen,line_Color,(0, HEIGHT - SQUARE_SIZE), (WIDTH, HEIGHT - SQUARE_SIZE), line_WIDTH)
+        
+    def next_player(self):
+        self.player = self.player % 2 + 1
 
 # main function
 def main():
     
     game = Game()
      
+    board = game.board
+    # mainLoop game
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:   
+                pos = event.pos
+                row = pos[1] // SQUARE_SIZE
+                col = pos[0] // SQUARE_SIZE
+                
+                # code for not filling not empty cells
+                if board.emptySquare(row,col):
+                    board.markSquare(row, col, game.player)
+                    game.next_player()
+                    print(board.squares)
+        
         
         pygame.display.update()
 
